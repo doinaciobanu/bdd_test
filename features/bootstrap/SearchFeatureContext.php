869e031a96b36891b6f91871a0ea15cb45f34164
Doina Ciobanu <ciobanudoina1990@gmail.com>
2018-02-07 19:33:10 -0700

    first commit

---
+++ features/bootstrap/SearchFeatureContext.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: igorciobanu
+ * Date: 2/7/18
+ * Time: 7:26 PM
+ */
+class SearchFeatureContext extends FeatureContext
+{
+    /**
+     * @Given /^I am on "([^"]*)"$/
+     */
+    public function iAmOn($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^I fill in "([^"]*)" with "([^"]*)"$/
+     */
+    public function iFillInWith($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^I press "([^"]*)"$/
+     */
+    public function iPress($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^I should see "([^"]*)"$/
+     */
+    public function iShouldSee($arg1)
+    {
+        throw new PendingException();
+    }
+
+}
